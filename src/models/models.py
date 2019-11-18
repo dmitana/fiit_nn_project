62780061a62585e9b9fa39ba6046687b463eb0c7
Denis Mitana <denis.mitana1@gmail.com>
2019-11-19 00:38:48 +0100

    CHANGE: refactor model hyperparameters

--- src/models/models.py
+++ src/models/models.py
@@ -5,7 +5,8 @@ from tensorflow.keras.optimizers import Adam
 from src.models.losses import YoloLoss
 
 
-def base_model(grid_size, input_shape=(256, 256, 3), n_categories=0):
+def base_model(grid_size, input_shape=(256, 256, 3), n_categories=0,
+               **hparams):
     """
     Base model for object detection using the YOLO method.
 
@@ -19,6 +20,14 @@ def base_model(grid_size, input_shape=(256, 256, 3), n_categories=0):
         images, without batch dimension.
     :param n_categories: int (default: 0), number of categories to be
         detected.
+    :param hparams: dict, training hyperparameters.
+        learning_rate: float (default: None), determines the step size
+            at each iteration step.
+        l_coord: float (default: 5.0), lambda coordinates parameter
+            for YOLO loss function. Weight of the XY and WH loss.
+        l_noobj: float (default: 0.5), lambda no object parameter for
+            YOLO loss function. Weight of the one part of the
+            confidence loss.
     :return: tf.keras.Model, compiled model.
     """
     (grid_rows, grid_cols) = grid_size
@@ -86,8 +95,12 @@ def base_model(grid_size, input_shape=(256, 256, 3), n_categories=0):
         name='base_model',
     )
     model.compile(
-        optimizer=Adam(learning_rate=0.0001),
-        loss=YoloLoss(grid_size),
+        optimizer=Adam(learning_rate=hparams['learning_rate']),
+        loss=YoloLoss(
+            grid_size,
+            l_coord=hparams['l_coord'],
+            l_noobj=hparams['l_noobj']
+        ),
     )
 
     return model
